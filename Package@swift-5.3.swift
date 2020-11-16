f330d89d3cb23b5f9c1c5e93cc510af8d63de0e6
Anastasia Kubova <v-ankubo@microsoft.com>
2020-11-16 17:02:02 +0300

    Add os check for Distribute module

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -21,11 +21,7 @@ let package = Package(
         .library(
             name: "AppCenterCrashes",
             type: .static,
-            targets: ["AppCenterCrashes"]),
-        .library(
-            name: "AppCenterDistribute",
-            type: .static,
-            targets: ["AppCenterDistribute"]),
+            targets: ["AppCenterCrashes"])
     ],
     dependencies: [
         .package(url: "https://github.com/microsoft/PLCrashReporter.git", .upToNextMinor(from: "1.8.0")),
@@ -82,24 +78,35 @@ let package = Package(
                 .linkedFramework("UIKit", .when(platforms: [.iOS, .tvOS])),
                 .linkedFramework("AppKit", .when(platforms: [.macOS])),
             ]
-        ),
-        .target(
-            name: "AppCenterDistribute",
-            dependencies: ["AppCenter"],
-            path: "AppCenterDistribute/AppCenterDistribute",
-            exclude: ["Support"],
-            resources: [
-                .process("Resources/AppCenterDistribute.strings"),
-            ],
-            cSettings: [
-                .headerSearchPath("**"),
-                .headerSearchPath("../../AppCenter/AppCenter/**"),
-            ],
-            linkerSettings: [
-                .linkedFramework("Foundation"),
-                .linkedFramework("SafariServices", .when(platforms: [.iOS])),
-                .linkedFramework("UIKit", .when(platforms: [.iOS])),
-            ]
         )
     ]
 )
+
+#if os(iOS)
+package.products += [
+    .library(
+        name: "AppCenterDistribute",
+        type: .static,
+        targets: ["AppCenterDistribute"])
+]
+package.targets += [
+    .target(
+        name: "AppCenterDistribute",
+        dependencies: ["AppCenter"],
+        path: "AppCenterDistribute/AppCenterDistribute",
+        exclude: ["Support"],
+        resources: [
+            .process("Resources/AppCenterDistribute.strings"),
+        ],
+        cSettings: [
+            .headerSearchPath("**"),
+            .headerSearchPath("../../AppCenter/AppCenter/**"),
+        ],
+        linkerSettings: [
+            .linkedFramework("Foundation"),
+            .linkedFramework("SafariServices", .when(platforms: [.iOS])),
+            .linkedFramework("UIKit", .when(platforms: [.iOS])),
+        ]
+    )
+]
+#endif
